9f9858cee7f21f39f0eca99eda82e697a72f0310
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-06-08 18:29:09 +0200

    ci(husky): setup ggshield & lint-staged

---
+++ scripts/dev/post_install.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
+import { B } from 'bhala'
+import { $ } from 'execa'
+import which from 'which'
+
+B.log('[Post Install Script] Checking required dependencies...')
+try {
+  which.sync('ggshield')
+} catch (error) {
+  B.warn(
+    '[Post Install Script] ggshield is not installed, please install it: https://github.com/GitGuardian/ggshield#installation.',
+  )
+}
+
+B.log('[Post Install Script] Installing Git hooks (husky)...')
+$`yarn husky`
