185fbf07323c4eb4eae8ba6587e849751735e996
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-08-28 01:08:35 +0200

    build(resources): move resources/ directory into src-tauri/

--- sidecars/src/common.rs
+++ sidecars/src/common.rs
@@ -5,18 +5,18 @@ use std::process::{exit, Command};
 pub fn resolve_resource_path(binary_name: &str) -> PathBuf {
     let current_exe = env::current_exe().expect("Failed to get current executable path");
     if current_exe.parent().unwrap().ends_with("target") {
-        // In development mode, use `../../../resources/clamav` as the base path
+        // In development mode, use `../../../src-tauri/resources/clamav` as the base path
         let mut dev_path = PathBuf::from(current_exe.parent().unwrap());
-        dev_path.pop(); // Go one level up
-        dev_path.pop(); // Go another level up
-        dev_path.pop(); // Go another level up
-        dev_path.push(format!("resources/clamav/{}", binary_name));
+        dev_path.pop();
+        dev_path.pop();
+        dev_path.pop();
+        dev_path.push(format!("src-tauri/resources/clamav/{}", binary_name));
 
         dev_path
     } else {
-        // In production mode, use `./_up_/resources` as the base path
+        // In production mode, use `./resources` as the base path
         let mut prod_path = PathBuf::from(current_exe.parent().unwrap());
-        prod_path.push(format!("_up_/resources/clamav/{}", binary_name));
+        prod_path.push(format!("resources/clamav/{}", binary_name));
 
         prod_path
     }
