463fdda1764d43a9dad105f8d7dd122fd5a84d29
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-08-29 00:11:26 +0200

    feat(core/cloud): finalize basic start_cloud_update command for all OS

--- src-tauri/src/cloud/commands.rs
+++ src-tauri/src/cloud/commands.rs
@@ -1,6 +1,5 @@
-use std::{env, process::Stdio};
+use std::process::Stdio;
 use tauri::{AppHandle, Manager, State};
-use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command as TokioCommand;
 
 use crate::debug;
@@ -25,150 +24,68 @@ pub async fn get_cloud_state(
         status,
     };
     *public_state_mutex_guard = next_public_state.clone();
-    app_handle
-        .emit_all("cloud:state", &next_public_state)
-        .unwrap();
+    app_handle.emit_all("cloud:state", &next_public_state).unwrap();
 
     Ok(())
 }
 
 #[cfg(not(tarpaulin_include))]
 #[tauri::command]
-pub async fn start_cloud_update(
-    app_handle: AppHandle,
-    shared_state: State<'_, state::CloudSharedState>,
-) -> Result<(), ()> {
+pub async fn start_cloud_update(app_handle: AppHandle) -> Result<(), ()> {
     use tauri::api::process::{Command, CommandEvent};
+
+    use crate::globals;
 
     debug!("start_cloud_update()", "Command call.");
 
-    let is_dev_mode = env::var("TAURI_DEV").is_ok();
+    let config_directory_path_mutex_guard = globals::CONFIG_DIRECTORY_PATH.lock().await;
 
-    if cfg!(target_os = "linux") {
-        // Update cloud state
-        let mut public_state_mutex_guard = shared_state.0.public.lock().await;
-        public_state_mutex_guard.is_running = true;
-        app_handle
-            .emit_all("cloud:state", &public_state_mutex_guard.clone())
-            .unwrap();
+    let config_directory_path = config_directory_path_mutex_guard.clone();
+    let freshclam_config_file_path = config_directory_path.join("freshclam.conf");
+    let freshclam_config_file_path_as_str = freshclam_config_file_path.to_str().unwrap();
 
-        let mut command = if is_dev_mode {
-            let mut cmd = TokioCommand::new("pkexec");
-            cmd.args(["freshclam", "--daemon-notify"]);
-            cmd
-        } else {
-            let mut cmd = TokioCommand::new("freshclam");
-            cmd.args(["--daemon-notify"]);
-            cmd
-        };
+    let (mut rx, _child) = Command::new_sidecar("freshclam")
+        .expect("failed to create `freshclam` binary command")
+        .args(["--config-file", freshclam_config_file_path_as_str])
+        .spawn()
+        .expect("Failed to spawn sidecar");
 
-        let mut child = command
-            .stderr(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed to spawn child process.");
-
-        let stdout = child
-            .stdout
-            .take()
-            .expect("Failed to attach standard output.");
-
-        let app_handle_clone_for_stdout = app_handle.clone();
-        let app_handle_clone_for_end = app_handle.clone();
-        tokio::spawn(async move {
-            let reader = BufReader::new(stdout);
-            let mut lines = reader.lines();
-            while let Ok(Some(line)) = lines.next_line().await {
-                #[cfg(debug_assertions)]
-                {
-                    println!("[libs::cli::run()] {}", line);
-                }
-
-                let mut public_state_mutex_guard = app_handle_clone_for_stdout
-                    .state::<state::CloudSharedState>()
-                    .inner()
-                    .0
-                    .public
-                    .lock()
-                    .await;
-                public_state_mutex_guard.logs.push(line);
-
-                app_handle_clone_for_stdout
-                    .emit_all("cloud:state", &public_state_mutex_guard.clone())
-                    .unwrap();
-            }
-
-            let _ = child.wait().await.expect("Failed to wait for child exit.");
-
-            // Update the state to indicate the process is no longer running
-            let mut public_state_mutex_guard = app_handle_clone_for_end
+    let app_handle_traveller = app_handle.clone();
+    tauri::async_runtime::spawn(async move {
+        while let Some(event) = rx.recv().await {
+            let mut public_state_mutex_guard = app_handle_traveller
                 .state::<state::CloudSharedState>()
                 .inner()
                 .0
                 .public
                 .lock()
                 .await;
-            public_state_mutex_guard.is_running = false;
 
-            // Updated cloud state
-            app_handle_clone_for_end
+            if let CommandEvent::Stdout(ref line) = event {
+                #[cfg(debug_assertions)]
+                {
+                    println!("[CommandEvent::Stdout] {}", line);
+                }
+
+                public_state_mutex_guard.logs.push(line.to_string());
+            }
+
+            if let CommandEvent::Stderr(ref line) = event {
+                #[cfg(debug_assertions)]
+                {
+                    println!("[CommandEvent::Stderr] {}", line);
+                }
+
+                public_state_mutex_guard.logs.push(line.to_string());
+            }
+
+            app_handle_traveller
                 .emit_all("cloud:state", &public_state_mutex_guard.clone())
                 .unwrap();
-        });
+        }
+    });
 
-        return Ok(());
-    }
-
-    if cfg!(target_os = "macos") {
-        return Ok(());
-    }
-
-    if cfg!(target_os = "windows") {
-        let (mut rx, _child) = Command::new_sidecar("freshclam")
-            .expect("failed to create `freshclam` binary command")
-            .spawn()
-            .expect("Failed to spawn sidecar");
-
-        let app_handle_clone_for_stdout = app_handle.clone();
-        tauri::async_runtime::spawn(async move {
-            // read events such as stdout
-            while let Some(event) = rx.recv().await {
-                let mut public_state_mutex_guard = app_handle_clone_for_stdout
-                    .state::<state::CloudSharedState>()
-                    .inner()
-                    .0
-                    .public
-                    .lock()
-                    .await;
-
-                if let CommandEvent::Stdout(ref line) = event {
-                    #[cfg(debug_assertions)]
-                    {
-                        println!("[CommandEvent::Stdout] {}", line);
-                    }
-
-                    public_state_mutex_guard.logs.push(line.to_string());
-                }
-
-                if let CommandEvent::Stderr(ref line) = event {
-                    #[cfg(debug_assertions)]
-                    {
-                        println!("[CommandEvent::Stderr] {}", line);
-                    }
-
-                    public_state_mutex_guard.logs.push(line.to_string());
-                }
-
-                app_handle_clone_for_stdout
-                    .emit_all("cloud:state", &public_state_mutex_guard.clone())
-                    .unwrap();
-            }
-        });
-
-        return Ok(());
-    }
-
-    panic!("Unsupported OS.");
+    Ok(())
 }
 
 #[cfg(not(tarpaulin_include))]
