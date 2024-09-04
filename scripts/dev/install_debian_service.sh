a932a74b3c1c1b77172106c1ba26dde049cabefd
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-04 04:21:29 +0200

    build(scripts): add install_debian_service

---
+++ scripts/dev/install_debian_service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Install binary & service file
+sudo cp ./clamav-desktop-daemon/target/release/clamav-desktop-daemon /usr/bin/clamav-desktop-daemon
+sudo cp ./src-tauri/embeds/deb/clamav-desktop-daemon.service /usr/lib/systemd/system/clamav-desktop-daemon.service
+
+# Reload systemd manager configuration
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable clamav-desktop-daemon.service
+sudo systemctl start clamav-desktop-daemon.service
+
+sudo systemctl status --no-pager clamav-desktop-daemon.service
