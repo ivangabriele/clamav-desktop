0ef9406d4a1a0d636a25306a0f34a5c7a8b42331
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-04 03:03:45 +0200

    build(core): move Debian desktop template & policy file into embeds

--- assets/deb/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp clamav-desktop-daemon /usr/local/bin/
-chmod +x /usr/local/bin/clamav-desktop-daemon
-
-systemctl enable clamav-desktop-daemon
-systemctl start clamav-desktop-daemon
-
-exit 0
