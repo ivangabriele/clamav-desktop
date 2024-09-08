4e1676fdc284daad2e9f41cc767b3441c1b8e187
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-08 16:14:11 +0200

    build(scripts): fix program name in install_deb_bundle

--- scripts/dev/install_deb_bundle.sh
+++ scripts/dev/install_deb_bundle.sh
@@ -4,7 +4,7 @@ set -e
 yarn release:deb
 # Allow `_apt` user to access the `deb` file
 mv ./src-tauri/target/release/bundle/deb/clamav-desktop_0.0.0_amd64.deb /tmp
-if dpkg -l | grep -qw twps-desktop; then
+if dpkg -l | grep -qw clamav-desktop; then
   sudo apt remove -y clamav-desktop || true
 fi
 sudo apt install -y /tmp/clamav-desktop_0.0.0_amd64.deb
