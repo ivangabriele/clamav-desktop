6007ca6086bdd91de292ec5c1b8607080e543316
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-08 11:43:26 +0200

    feat(core/dashboard): add naive logged daemon ping

--- src-tauri/src/dashboard/commands.rs
+++ src-tauri/src/dashboard/commands.rs
@@ -1,6 +1,11 @@
+use futures_util::{SinkExt, StreamExt};
 use std::process::Stdio;
 use tauri::{AppHandle, Manager, State};
 use tokio::process::Command;
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{http::Uri, Message},
+};
 
 use crate::debug;
 
@@ -14,20 +19,55 @@ pub async fn get_dashboard_state(
 ) -> Result<(), ()> {
     debug!("get_dashboard_state()", "Command call.");
 
-    let mut public_state_mutex_guard = shared_state.0.public.lock().await;
+    let url = Uri::from_static("ws://0.0.0.0:7878");
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    let (mut write, mut read) = ws_stream.split();
 
-    let (status, logs) = utils::get_service_status();
-    let next_public_state = state::DashboardPublicState {
-        is_ready: true,
-        logs,
-        status,
+    let client_message = types::ClientMessage {
+        id: cuid::cuid1().expect("Failed to generate CUID."),
+        action: "Ping".to_string(),
+        data: serde_json::json!({}),
     };
-    *public_state_mutex_guard = next_public_state.clone();
-    app_handle
-        .emit_all("dashboard:state", &next_public_state)
-        .unwrap();
+    let client_message_as_string = serde_json::to_string(&client_message).unwrap();
+    write
+        .send(Message::Text(client_message_as_string))
+        .await
+        .expect("Failed to send status command");
 
-    Ok(())
+    match read.next().await {
+        Some(Ok(daemon_message)) => {
+            let daemon_message_as_text = daemon_message
+                .to_text()
+                .expect("Failed to convert `daemon_message` to text.")
+                .to_string();
+            let daemon_message: types::DaemonMessage = serde_json::from_str(&daemon_message_as_text)
+                .expect("Failed to convert `daemon_message_as_text` to `DaemonMessage`.");
+            println!("Received a message from daemon: `{:?}`.", daemon_message);
+
+            let mut public_state_mutex_guard = shared_state.0.public.lock().await;
+
+            let (status, logs) = utils::get_service_status();
+            let next_public_state = state::DashboardPublicState {
+                is_ready: true,
+                logs,
+                status,
+            };
+            *public_state_mutex_guard = next_public_state.clone();
+            app_handle.emit_all("dashboard:state", &next_public_state).unwrap();
+
+            return Ok(());
+        }
+        Some(Err(e)) => {
+            println!("Error: {:?}", e);
+
+            return Err(());
+        }
+        None => {
+            println!("No message received.");
+
+            return Err(());
+        }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
--- src-tauri/src/dashboard/mod.rs
+++ src-tauri/src/dashboard/mod.rs
@@ -1,3 +1,4 @@
 pub mod commands;
 pub mod state;
+mod types;
 mod utils;
