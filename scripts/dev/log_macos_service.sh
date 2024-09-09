729968780d714b364a414e66aa8f00a15919e71f
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-10 01:48:17 +0200

    build(scripts): use Unix FSH instead of macos Unified Logs in dev/log_macos_service

--- scripts/dev/log_macos_service.sh
+++ scripts/dev/log_macos_service.sh
@@ -12,16 +12,16 @@ must_be_root
 log.log "Getting macOS service status..."
 launchctl list | grep com.clamav-desktop.daemon
 
-log.log "Tailing macOS service logs... (Press Ctrl+C to stop)"
-log stream --predicate "process == \"${UNIX_BINARY_NAME}\"" --info
-# if [ -f "${UNIX_STDOUT_LOG_PATH}" ] && [ -f "${UNIX_STDERR_LOG_PATH}" ]; then
-#     log.log "Tailing macOS service logs... (Press Ctrl+C to stop)"
-#     tail -f "$UNIX_STDOUT_LOG_PATH" "$UNIX_STDERR_LOG_PATH"
-# else
-#     if [ ! -f "${UNIX_STDOUT_LOG_PATH}" ]; then
-#         log.error "Log file '${UNIX_STDERR_LOG_PATH}' does not exist."
-#     fi
-#     if [ ! -f "${UNIX_STDERR_LOG_PATH}" ]; then
-#         log.error "Error log file '${UNIX_STDERR_LOG_PATH}' does not exist."
-#     fi
-# fi
+# log.log "Tailing macOS service logs... (Press Ctrl+C to stop)"
+# log stream --predicate "process == \"${UNIX_BINARY_NAME}\"" --info
+if [ -f "${UNIX_STDOUT_LOG_PATH}" ] && [ -f "${UNIX_STDERR_LOG_PATH}" ]; then
+    log.log "Tailing macOS service logs... (Press Ctrl+C to stop)"
+    tail -f "$UNIX_STDOUT_LOG_PATH" "$UNIX_STDERR_LOG_PATH"
+else
+    if [ ! -f "${UNIX_STDOUT_LOG_PATH}" ]; then
+        log.error "Log file '${UNIX_STDERR_LOG_PATH}' does not exist."
+    fi
+    if [ ! -f "${UNIX_STDERR_LOG_PATH}" ]; then
+        log.error "Error log file '${UNIX_STDERR_LOG_PATH}' does not exist."
+    fi
+fi
