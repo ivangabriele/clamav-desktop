115edb5b0dd7e93cc8ecaf4e0f3925c8edbc1641
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-13 18:05:01 +0200

    feat(webview/components): add KeyValueList

---
+++ src/components/KeyValueList.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const RawKeyValueList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Reddit Mono', monospace;
+  font-size: 70%;
+  padding: 0;
+`
+
+const Row = styled.li`
+  display: flex;
+`
+
+const Key = styled.span
+
+const Value = styled.span`
+  flex-grow: 1;
+  text-align: right;
+`
+
+export const KeyValueList = Object.assign(RawKeyValueList, {
+  Key,
+  Row,
+  Value,
+})
