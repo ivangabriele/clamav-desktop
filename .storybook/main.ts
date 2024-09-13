ac784fc7e63010d428b38cfc7cba59b8316df545
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-13 08:54:46 +0200

    build(storybook): disable Vite server HMR overlay

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,24 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
   staticDirs: ['../public'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
+  async viteFinal(config) {
+    const { mergeConfig } = await import('vite')
+
+    return mergeConfig(config, {
+      server: {
+        hmr: {
+          overlay: false,
+        },
+      },
+    })
+  },
 }
 export default config
