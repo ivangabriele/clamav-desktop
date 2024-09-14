28d85456f0f8730c75338634d69c10471a3c4c5d
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-14 18:54:27 +0200

    feat(webview/layouts): allow both grid & flex in ScreenBox

--- src/layouts/ScreenBox.tsx
+++ src/layouts/ScreenBox.tsx
@@ -1,22 +1,43 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import type { ReactNode } from 'react'
 
 type ScreenBoxProps = {
   children: ReactNode
+  isCentered?: boolean
+  isGrid?: boolean
 }
-export function ScreenBox({ children }: ScreenBoxProps) {
-  return <Box>{children}</Box>
+export function ScreenBox({ children, isCentered = false, isGrid = false }: ScreenBoxProps) {
+  return (
+    <Box $isCentered={isCentered} $isGrid={isGrid}>
+      {children}
+    </Box>
+  )
 }
 
 const Box = styled.div.attrs({ className: 'Screen' })<{
-  $isLoading?: boolean
+  $isCentered: boolean
+  $isGrid: boolean
 }>`
-  column-gap: 16px;
-  display: grid;
   flex-grow: 1;
-  grid-template-columns: 200px 200px 280px;
-  grid-template-rows: 200px 200px;
-  padding: 16px 16px 16px 0;
-  row-gap: 16px;
+
+  ${p =>
+    !p.$isGrid &&
+    css`
+      align-items: ${p.$isCentered ? 'center' : 'flex-start'};
+      display: flex;
+      flex-direction: column;
+      justify-content: ${p.$isCentered ? 'center' : 'flex-start'};
+      padding: 0px 88px 16px 16px;
+  `}
+  ${p =>
+    p.$isGrid &&
+    css`
+      column-gap: 16px;
+      display: grid;
+      grid-template-columns: 200px 200px 280px;
+      grid-template-rows: 200px 200px;
+      padding: 16px 16px 16px 0;
+      row-gap: 16px;
+  `}
 `
