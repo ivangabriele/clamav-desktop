9549f2904e99272a9c19187bb6dfec60c4775916
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-14 06:55:57 +0200

    feat(webview/screens): add daemon logs in next Dashboard component

--- src/stories/screens/DashboardPage.stories.tsx
+++ src/stories/screens/DashboardPage.stories.tsx
@@ -1,10 +1,21 @@
+import { faker } from '@faker-js/faker'
 import type { Meta, StoryObj } from '@storybook/react'
+
+import { argsStore } from '../../../.storybook/argsStore'
 import { ArgStoreKey } from '../../../.storybook/argsStore/constants'
 import { useArgsStoreArgs } from '../../../.storybook/argsStore/useArgsStoreArgs'
 import { Layout } from '../../Layout'
-import { Page } from '../../constants'
+import { Screen } from '../../constants'
 import { DashboardScreenComponent, type DashboardScreenComponentProps } from '../../screens/Dashboard/Component'
+import { Core } from '../../types'
 import { noop } from '../../utils/noop'
+import { waitFor } from '../../utils/waitFor'
+
+const FAKE_DAEMON_LOGS: Core.Log[] = Array.from({ length: 100 }, () => ({
+  date: faker.date.recent().toISOString(),
+  message: faker.hacker.phrase(),
+  type: faker.helpers.arrayElement(['stderr', 'stdout']),
+})).sort((a, b) => +new Date(a.date) - +new Date(b.date))
 
 const meta = {
   title: 'Screens/Dashboard',
@@ -25,14 +36,26 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     daemonClientState: undefined,
+    daemonLogs: undefined,
+  },
+  play: async () => {
+    await waitFor(1000)
+
+    argsStore.updateArgs<DashboardScreenComponentProps>(ArgStoreKey.DashboardScreenComponent, {
+      daemonClientState: {
+        daemon_status: Core.DaemonStatus.Running,
+      },
+      daemonLogs: FAKE_DAEMON_LOGS,
+    })
   },
   render: () => {
     const [args] = useArgsStoreArgs<DashboardScreenComponentProps>(ArgStoreKey.DashboardScreenComponent, {
       daemonClientState: undefined,
+      daemonLogs: undefined,
     })
 
     return (
-      <Layout onPageChange={noop} page={Page.Dashboard}>
+      <Layout onScreenChange={noop} activeScreen={Screen.Dashboard}>
         <DashboardScreenComponent {...args} />
       </Layout>
     )
