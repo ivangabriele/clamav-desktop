4c982290cee9fc39d1afbdce23c2ea049326ff71
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-15 02:32:38 +0200

    feat(webview/components): add new FileExplorer

---
+++ src/components/FileExplorer/Checkbox.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+import { type MouseEvent, useCallback } from 'react'
+import { MdAdd, MdCheck, MdRemove } from 'react-icons/md'
+import { TreeNodeCheckState } from './constants'
+
+interface CheckboxProps {
+  onToggle: (nextState: TreeNodeCheckState) => void
+  state: TreeNodeCheckState
+}
+export function Checkbox({ onToggle, state }: Readonly<CheckboxProps>) {
+  const isChecked = state !== TreeNodeCheckState.Unchecked
+
+  const toggle = useCallback(
+    (event: MouseEvent<HTMLDivElement>) => {
+      event.stopPropagation()
+
+      const nextState = state === TreeNodeCheckState.Checked ? TreeNodeCheckState.Unchecked : TreeNodeCheckState.Checked
+
+      onToggle(nextState)
+    },
+    [onToggle, state],
+  )
+
+  return (
+    <Box $isChecked={isChecked} onClick={toggle}>
+      {state === TreeNodeCheckState.Unchecked && <MdAdd size={20} />}
+      {state === TreeNodeCheckState.PartiallyChecked && <MdRemove size={20} />}
+      {state === TreeNodeCheckState.Checked && <MdCheck size={20} />}
+    </Box>
+  )
+}
+
+export const Box = styled.span<{
+  $isChecked: boolean
+}>`
+  align-items: center;
+  background-color: ${p => (p.$isChecked ? '#006633' : 'transparent')};
+  cursor: pointer;
+  display: flex;
+  height: 26px;
+  justify-content: center;
+  width: 26px;
+
+  &:hover {
+    background-color: gold;
+    color: black;
+  }
+
+  * {
+    cursor: pointer;
+  }
+`
