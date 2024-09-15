a14d880b82236e414722212ca563c66be3375155
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-15 17:37:05 +0200

    feat(core/filer): add FileList::into_file_paths() with FilePath type

--- src-tauri/filer/src/lib.rs
+++ src-tauri/filer/src/lib.rs
@@ -1,15 +1,5 @@
-// mod file_explorer;
-// mod file_list;
-// mod types;
-// mod utils;
-
 pub mod drive;
 pub mod file_explorer;
 pub mod file_list;
 pub mod types;
 pub mod utils;
-// pub use crate::file_explorer::{FileExplorer, FileExplorerTree};
-// pub use crate::file_list::count;
-// pub use crate::file_list::list;
-// pub use crate::types::FileKind;
-// pub use crate::utils::normalize_path;
